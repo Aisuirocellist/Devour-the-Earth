315d53bd3c632caba5157f551c146eba81f00169
Nirow <danielseyunlee@gmail.com>
2025-04-05 20:06:18 -0400

    Movement Fix

--- Assets/Scripts/RelativeMovement.cs
+++ Assets/Scripts/RelativeMovement.cs
@@ -6,25 +6,24 @@ public class RelativeMovement : MonoBehaviour
     public float maxSpeed = 5;
     public float accelRate = 1.2f;
     public float decelRate = 1.2f;
-    private Vector2 velocity;
+
+    private Vector2 moveDir;
     private Rigidbody2D rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        velocity = new Vector2(0, 0);
+        moveDir = new Vector2(0, 0);
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
+        moveDir.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveDir.Normalize();
 
-        Vector2 targetVelocity = new Vector2(inputX * maxSpeed, inputY * maxSpeed);
-        velocity += (targetVelocity - velocity) * ((inputX == 0 && inputY == 0) ? decelRate : accelRate);
-
-        transform.Translate(velocity * Time.deltaTime, Space.World);
+        Vector2 moveForce = (moveDir * maxSpeed - rb.linearVelocity) * (moveDir.magnitude > 0 ? accelRate : decelRate);
+        rb.AddForce(moveForce, ForceMode2D.Force);
     }
 }
