1e4ec01248c2d47556b81c8dd691fc1a5c949429
James-275 <jmf275@case.edu>
2025-04-22 01:17:14 -0400

    Continuous Shooter

---
+++ Assets/Scripts/ContinuousShooterAI.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+[RequireComponent (typeof(EnemyPathfinding))]
+public class ContinuousShooter : BulletSkill
+{
+    [SerializeField] EnemyBullet bulletPrefab;
+    private EnemyPathfinding enemyPathfinding;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected override void Start()
+    {
+        base.Start();
+        enemyPathfinding = GetComponent<EnemyPathfinding>();
+    }
+
+    public override void UseSkill()
+    {
+        EnemyBullet bullet = Instantiate(bulletPrefab, bulletFirePoint.position, transform.rotation);
+
+        bullet.SetDamage(GetDamage());
+        bullet.SetSpeed(bulletSpeed);
+
+        base.UseSkill();
+    }
+
+    protected override bool IsSkillUseTriggered()
+    {
+        return true;
+    }
+}
